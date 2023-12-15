49a72f1a6ab2131ac84243cee62563cad289fbcd
Philipp Albrecht <philipp@uisa.ch>
2023-12-15 02:11:26 +0100

    added PersistentKeepalive

--- helpers.py
+++ helpers.py
@@ -8,5 +8,5 @@ def extract_ip(str):
 
 def get_wireguard_config_file_str(private_key, address, public_key, allowed_ips, endpoint):
     return (f"[Interface]\nPrivateKey = {private_key}\nAddress = {address}\n\n[Peer]\nPublicKey = {public_key}\n"
-            f"AllowedIPs = {allowed_ips}\nEndpoint = {endpoint}\n")
+            f"AllowedIPs = {allowed_ips}\nEndpoint = {endpoint}\nPersistentKeepalive = 25\n")
 
